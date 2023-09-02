296e4f582462a85f541d3bf9a663c8c70dd3b7c7
Aniket <akeshri@nownow.ng>
2023-09-02 14:00:32 +0530

    Changes Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.chaos.view:pinview:1.4.3") //code picker
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
