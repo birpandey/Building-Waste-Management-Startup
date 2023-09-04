e3d7d92303cf295a3b7b588379ca9dc6e6c05d4f
Birpandey <birpandey3@gmail.com>
2023-09-05 00:32:10 +0530

    added recyclerview

--- app/src/main/java/com/example/waste/fragments/Home.kt
+++ app/src/main/java/com/example/waste/fragments/Home.kt
@@ -1,15 +1,15 @@
 package com.example.waste.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.example.waste.databinding.FragmentHomeBinding
+import androidx.navigation.Navigation.findNavController
 import com.bumptech.glide.Glide
-import com.example.waste.adapter.EwasteItems
+import com.example.waste.R
+import com.example.waste.databinding.FragmentHomeBinding
 
 class Home : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -26,30 +26,28 @@ class Home : Fragment() {
         return rootView
     }
 
-    private fun initialize(){
-        binding.cvEwaste.setOnClickListener {
-            val intent = Intent(requireContext(), EwasteItems::class.java)
-            startActivity(intent)
+    private fun initialize() {
+        binding.cvEwaste.setOnClickListener { view ->
+            findNavController(view).navigate(R.id.wasteItems)
         }
 
-        binding.cvPlastic.setOnClickListener {
-            val intent = Intent(requireContext(), EwasteItems::class.java)
-            startActivity(intent)
+        binding.cvPlastic.setOnClickListener { view ->
+            findNavController(view).navigate(R.id.wasteItems)
         }
 
-        binding.cvBattery.setOnClickListener {
-            val intent = Intent(requireContext(), EwasteItems::class.java)
-            startActivity(intent)
+        binding.cvBattery.setOnClickListener { view ->
+            findNavController(view).navigate(R.id.wasteItems)
         }
 
-        binding.cvTyre.setOnClickListener {
-            val intent = Intent(requireContext(), EwasteItems::class.java)
-            startActivity(intent)
+        binding.cvTyre.setOnClickListener { view ->
+            findNavController(view).navigate(R.id.wasteItems)
         }
     }
-    private fun bannerWebView(){
+
+    private fun bannerWebView() {
         val imageUrl: ImageView = binding.imageView
-        val url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAOZsOIBTyNND54nzK7qntwTYvOXjSz-m4yg&usqp=CAU"
+        val url =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAOZsOIBTyNND54nzK7qntwTYvOXjSz-m4yg&usqp=CAU"
         Glide.with(this).load(url).into(imageUrl)
     }
 }
---
+++ app/src/main/java/com/example/waste/models/PriceWasteItemModel.kt
@@ -0,0 +1,7 @@
+package com.example.waste.models
+
+data class PriceWasteItemModel(
+    val price:String,
+    val productImage:String,
+    val productName:String
+)
