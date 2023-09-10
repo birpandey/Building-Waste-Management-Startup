83f4375ea0189c66d9b289fd3cf5679b6162bf90
abhishek <pandeyabhishek319@gmail.com>
2023-09-10 12:15:10 +0530

    1. Circular recyclerview done

--- app/src/main/java/com/example/waste/adapter/BannerAdapter.kt
+++ app/src/main/java/com/example/waste/adapter/BannerAdapter.kt
@@ -1,34 +1,44 @@
 package com.example.waste.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.waste.R
+import com.example.waste.databinding.BannerItemBinding
 import com.example.waste.models.BannerItem
-import com.squareup.picasso.Picasso
 
-class BannerAdapter(private val bannerItems: List<BannerItem>) :
+class BannerAdapter(private val context: Context, private val bannerItems: List<BannerItem>) :
     RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
 
-    inner class BannerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val bannerImage: ImageView = itemView.findViewById(R.id.banner_image)
+    lateinit var binding :BannerItemBinding
+
+    inner class BannerViewHolder(val binding: BannerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.banner_item, parent, false)
-        return BannerViewHolder(view)
+        binding =  BannerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return BannerViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
-        val item = bannerItems[position]
+
+        val realPosition = position%listSize()
+        val user = bannerItems[realPosition]
         // Load the image into the ImageView using a library like Picasso or Glide
         // For example, using Picasso:
-        Picasso.get().load(item.imageUrl).into(holder.bannerImage)
+        Glide.with(context).load(user.imageUrl).into(holder.binding.bannerImage)
     }
 
     override fun getItemCount(): Int {
-        return bannerItems.size
+        return 3 * bannerItems.size
+    }
+
+    public fun listSize():Int{
+        return bannerItems.size        // original list size
     }
 }
--- app/src/main/java/com/example/waste/fragments/Home.kt
+++ app/src/main/java/com/example/waste/fragments/Home.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.waste.R
 import com.example.waste.adapter.BannerAdapter
@@ -44,17 +46,42 @@ class Home : Fragment() {
             findNavController(view).navigate(R.id.wasteItems)
         }
     }
-
+lateinit var bannerAdapter: BannerAdapter
     private fun banner() {
-        val bannerRecyclerView: RecyclerView = requireView().findViewById(R.id.banner_recycler_view)
+//        val bannerRecyclerView: RecyclerView = requireView().findViewById(R.id.banner_recycler_view)
         val bannerItems = listOf(
             BannerItem("https://media.istockphoto.com/id/1293353471/photo/recycling-symbol-abstract-concept-3d-illustration.jpg?s=1024x1024&w=is&k=20&c=f-K28sQgRGVLHgFUqf4xsOttLR944FgNaTMsaiu7PfY="),
             BannerItem("https://media.istockphoto.com/id/1293353502/photo/recycling-symbol-abstract-concept-3d-illustration.jpg?s=1024x1024&w=is&k=20&c=ambvpy-DifkF4WYb6WJKadaXRsu7RB8e2TsFtxdXDvM="),
             BannerItem("https://media.istockphoto.com/id/1273205088/photo/cyber-monday-abstract-3d-illustration.jpg?s=1024x1024&w=is&k=20&c=3aDT6XZk66OfVjm5jmLODyTDAJutaUhNxW5FcBrvbh4=")
             // Add more banner items as needed
         )
-        val bannerAdapter = BannerAdapter(bannerItems)
-        bannerRecyclerView.adapter = bannerAdapter
+        bannerAdapter = BannerAdapter(requireContext(), bannerItems)
+       /* val pagerSnapHelper = PagerSnapHelper()
+        pagerSnapHelper.attachToRecyclerView(binding.bannerRecyclerView)*/
+        binding.bannerRecyclerView.adapter = bannerAdapter
+        setAdapater()
 
+    }
+
+
+
+    fun setAdapater(){
+        val listSize = bannerAdapter.listSize()
+        binding.bannerRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val firstVisiblePosition = layoutManager.findFirstVisibleItemPosition()
+                if (firstVisiblePosition > listSize && firstVisiblePosition%listSize == 0 ) {
+                    // when the position reaches to the end of the list, we will scroll to begining of the list.
+                    recyclerView.scrollToPosition(listSize)
+                } else if (firstVisiblePosition == listSize - 1) {
+                    // when position reaches to the begining of list, then we need to scroll to list*2(end of list).
+                    recyclerView.scrollToPosition(listSize*2)
+                }
+            }
+        })
+        val pagerSnapHelper = PagerSnapHelper()
+        pagerSnapHelper.attachToRecyclerView(binding.bannerRecyclerView)
     }
 }
